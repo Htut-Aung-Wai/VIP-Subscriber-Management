bfd6595eb7f5bbcd5f201377c18fdf1e8cc7f7c8
Myat Thukha <thukhamyat2000@gmail.com>
2025-10-08 16:41:50 +0630

    unithead

---
+++ src/main/java/com/mytel/ciao/vip_subscriber_management/vip_subscriber_management/repository/UnitHeadRepo.java
@@ -0,0 +1,8 @@
+package com.mytel.ciao.vip_subscriber_management.vip_subscriber_management.repository;
+
+import com.mytel.ciao.vip_subscriber_management.vip_subscriber_management.entity.UnitHead;
+
+public interface UnitHeadRepo extends JpaRepository<UnitHead, Long> {
+
+
+}
