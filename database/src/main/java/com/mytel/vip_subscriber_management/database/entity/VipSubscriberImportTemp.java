b2c09e9061520d37efff2e7d8c75508052bc4494
Htut-Aung-Wai <htutaungwai333@gmail.com>
2025-11-06 08:45:08 +0630

    merge : git merge

--- database/src/main/java/com/mytel/vip_subscriber_management/database/entity/VipSubscriberImportTemp.java
+++ database/src/main/java/com/mytel/vip_subscriber_management/database/entity/VipSubscriberImportTemp.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "vip_subscriber_import_temp")
+@Table(name = "CAIO_VIP_VIP_SUBSCRIBER_IMPORT_TEMP")
 @Getter
 @Setter
 public class VipSubscriberImportTemp extends BaseEntity{
